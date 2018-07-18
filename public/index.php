a17bb414cafc17ebda71e280aaaabdc7fb1d226e
unknown <grantjhanrahan@gmail.com>
2018-07-18 10:35:47 +1000

    added create.php form

--- public/index.php
+++ public/index.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP Database</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <h1>Simple Database App</h1>
-
+<?php include "templates/header.php"; ?>
     <ul>
         <li><a href="create.php"><strong>Create</strong></a> - add user</li>
         <li><a href="read.php"><strong>Read</strong></a> - find a user</li>
     </ul>
-    
-</body>
-</html>
+<?php include "templates/footer.php"; ?>
