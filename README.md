1dffbf62f4d6a41f037e1b1c9d7e8a1eb0291c38
unknown <grantjhanrahan@gmail.com>
2018-07-19 09:00:44 +1000

    added DELETE function of CRUD

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### _[PHP + SQL tutorial - Create a Simple CRUD Database App: Connecting to MySQL with PHP]_(https://www.taniarascia.com/create-a-simple-database-app-connecting-to-mysql-with-php/)
+### PHP + SQL tutorial
+
+ [Create a Simple CRUD Database App: Connecting to MySQL with PHP](https://www.taniarascia.com/create-a-simple-database-app-connecting-to-mysql-with-php/)
--- common.php
+++ common.php
@@ -9,3 +9,16 @@
      return htmlspecialchars($html, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
  }
 //  With this function, any variable can be wrapped in escape() and the HTML entities will be protected
+
+
+session_start();
+
+if (empty($_SESSION['csrf'])) {
+    if (function_exists('random_bytes')) {
+        $_SESSION['csrf'] = bin2hex(random_bytes(32));
+    } else if (function_exists('mcrypt_create_iv')) {
+        $_SESSION['csrf'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+    } else {
+        $_SESSION['csrf'] = bin2hex(openssl_random_pseudo_bytes(32));
+    }
+}
